c8a4b4c1e878f4d441976198d16ae308cbe9ca00
Omar Tantawy <YOUR_GITHUB_EMAIL>
2026-08-31 12:48:02 +0300

    2 Problems

--- Task_1_C#_ITI/Program.cs
+++ Task_1_C#_ITI/Program.cs
@@ -4,7 +4,74 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Task_1_D01
+
+            ///*
+            // •	Consider an Array of Integer values with size N, having values as in this
+            //    Example 
+
+            //    7	0	0	0	5	6	7	5	0	7	5	3
+
+            //    Your task will be to write a program find the longest distance between
+            //    Two equal cells. In this example. The distance is measured by the number
+            //    Of cells- for example, the distance between the first and the fourth cell is
+            //    2 (cell 2 and cell 3).
+            //    In the example above, the longest distance is between the first 7 and the
+            //    10th 7, with a distance of 8 cells, i.e. the number of cells between the 1st
+            //    And the 10th 7s.
+            //    Note:
+            //    - Array values will be taken from the user
+            //    - If you have input like 1111111 then the distance is the number of
+            //    Cells between the first and the last cell.
+
+
+            // */
+
+            //Console.WriteLine("Please Enter The Number Of Array");
+
+            //int X = Convert.ToInt32(Console.ReadLine());
+
+            //int count = 0;
+
+            //int[] arr = new int[X];
+            //for (int z = 0; z < arr.Length; z++)
+            //{
+            //    arr[z] = Convert.ToInt32(Console.ReadLine());
+
+            //}
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    for (int j = i+1; j < arr.Length; j++)
+            //    {
+            //        if (arr[i] == arr[j])
+            //        {
+            //            Console.WriteLine($"{arr[i]} Equall {arr[j]} : Distance = {count}");
+
+            //        }
+
+            //        count++;
+            //    }
+
+            //    count =0;
+            //}
+
+            #endregion
+
+
+            //#region Task2_D01
+            //string input = Console.ReadLine();
+
+            //string[] words = input.Split(' ');
+
+            //Array.Reverse(words);
+
+            //Console.WriteLine(string.Join(" ", words)); 
+            //#endregion
+
+
+
+
         }
     }
 }
